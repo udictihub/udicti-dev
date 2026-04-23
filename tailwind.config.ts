a8a725bcf8148fc0cf168c32a07069d7459ff059
sirily23 <sirilylasway@gmail.com>
2026-04-23 20:22:22 +0300

    remove default Tailwind colors to enforce brand palette

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,20 +6,7 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {
-    extend: {
-      colors: {
-        primary: {
-          DEFAULT: '#0864AF',
-          dark: '#064A85',
-        },
-        secondary: {
-          DEFAULT: '#FFD700',
-          dark: '#CCAC00',
-        },
-      },
-    },
-  },
+ 
   plugins: [require('@tailwindcss/typography')],
 };
 
